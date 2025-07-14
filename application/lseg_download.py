83eeb2c81609e16a60b81dca80ae0df966f57104
Emir Balcı <emirbalci360@gmail.com>
2025-07-14 18:42:04 +0300

    Fix checkpoint directory paths

--- application/lseg_download.py
+++ application/lseg_download.py
@@ -2,8 +2,8 @@ import gdown
 import os
 
 def main():
-    checkpoint_dir = "/Bench_LLM_Nav/vlmaps/lseg/checkpoints"
-    checkpoint_path = "/Bench_LLM_Nav/vlmaps/lseg/checkpoints/demo_e200.ckpt"
+    checkpoint_dir = "/Bench_LLM_Nav/application/semantic/checkpoints"
+    checkpoint_path = "/Bench_LLM_Nav/application/semantic/checkpoints/demo_e200.ckpt"
     checkpoint_url = "https://drive.google.com/u/0/uc?id=1ayk6NXURI_vIPlym16f_RG3ffxBWHxvb"
     os.makedirs(checkpoint_dir, exist_ok=True)
     gdown.download(checkpoint_url, output=str(checkpoint_path))
