9d89a0a900b64c00578f0039c111687211cff57d
Emir Balcı <emirbalci360@gmail.com>
2025-10-25 16:09:12 +0200

    Update hloc version
    Switch to a compatible commit

--- install.bash
+++ install.bash
@@ -6,4 +6,8 @@ conda install habitat-sim=0.2.2 -c conda-forge -c aihabitat -y
 cd ~
 git clone --recursive https://github.com/cvg/Hierarchical-Localization/
 cd Hierarchical-Localization/
+
+# switch to a compatible version of hloc, see the commit: https://github.com/cvg/Hierarchical-Localization/commit/936040e8d67244cc6c8c9d1667701f3ce87bf075
+git checkout 936040e8d67244cc6c8c9d1667701f3ce87bf075 
+
 python -m pip install -e .
